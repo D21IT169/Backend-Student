ef9943745ba6709a4ea0fd1d68d5abcd82e37efd
D21IT169 <d21it169@charusat.edu.in>
2022-09-12 13:27:45 +0530

    Backend Part

---
+++ src/main/java/in/ac/charusat/studentmgmtsystem/controller/StudentController.java
@@ -0,0 +1,47 @@
+package in.ac.charusat.studentmgmtsystem.controller;
+
+
+import in.ac.charusat.studentmgmtsystem.model.Student;
+import in.ac.charusat.studentmgmtsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins="http://localhost:3000")
+public class StudentController {
+
+    @Autowired
+    StudentRepository studentRepository;
+    @GetMapping("/listStudents")
+    public List<Student> getAllStudents() { return studentRepository.findAll(); }
+
+    @GetMapping("/student/{id}")
+    public Student getStudent(@PathVariable Integer id) {
+        return studentRepository.findById(id).get();
+    }
+
+    @DeleteMapping("/student/{id}")
+    public List<Student> deleteStudent(@PathVariable Integer id) {
+        studentRepository.delete(studentRepository.findById(id).get());
+        return studentRepository.findAll();
+    }
+
+    @PostMapping("/student")
+    public List<Student> addStudent(@RequestBody Student student) {
+        studentRepository.save(student);
+        return studentRepository.findAll();
+    }
+    @PutMapping("/student/{id}")
+    public List<Student> updateStudent(@RequestBody Student student, @PathVariable Integer id) {
+        Student studentObj = studentRepository.findById(id).get();
+        studentObj.setName(student.getName());
+        studentObj.setAddress(student.getAddress());
+        studentRepository.save(studentObj);
+        return studentRepository.findAll();
+    }
+
+}
